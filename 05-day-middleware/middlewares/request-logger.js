7c927c3f308b7702e9c10d80fa224dd20e3b4df8
Slawomir Jakub <53911551+mirJakub@users.noreply.github.com>
2024-05-28 15:55:18 +0200

    Delete 05-day-middleware directory

--- 05-day-middleware/middlewares/request-logger.js
+++
@@ -1,7 +0,0 @@
-// request logger middleware
-const requestLogger = (req, res, next) => {
-    console.log(`${req.method} ${req.path}`);
-    next(); // Important! Pass control to next middleware
-};
-
-export default requestLogger;
